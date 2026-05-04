7f8a3492b75fe2d8aa9f19203d0f2aced9c75ed6
Jonah Reigle <jonah.reigle@etudiant.univ-rennes.fr>
2026-05-04 20:03:52 +0200

    grosse amelioration et début des horaires

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ main = Blueprint('main', __name__)
 @main.route("/")
 def home():
     return render_template("index.html")
+
+@main.route("/horaires")
+def horaires():
+    return render_template("horaires.html")
