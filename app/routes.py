2ed2a586d5dc3bd8c0874ad9ec3c41ab16b45558
Jonah Reigle <jonah.reigle@etudiant.univ-rennes.fr>
2026-05-11 14:03:47 +0200

    bientot la compet

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,14 @@ main = Blueprint('main', __name__)
 def home():
     return render_template("index.html")
 
+@main.route("/club")
+def club():
+    return render_template("club.html")
+
 @main.route("/horaires")
 def horaires():
     return render_template("horaires.html")
+
+@main.route("/calendrier-competitions")
+def calendrier_competitions():
+    return render_template("calendrier_competitions.html")
